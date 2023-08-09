ee8b32fefd142a5f70ca835f18c6bbc1927274cd
Joshua Pease <joshua.pease@viget.com>
2023-08-08 17:12:04 -0700

    Working add to cart

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use craft\commerce\elements\Product;
+
 use function markhuot\craftpest\helpers\http\get;
 
 //
@@ -12,3 +14,26 @@ use function markhuot\craftpest\helpers\http\get;
 //
 // Before any of that, let's start with a simple test ensures the homepage loads and returns an 200 Ok status code.
 get('/')->assertOk();
+
+it('does-the-thing', function() {
+    $product = Product::find()->one();
+
+//    get($product->url)
+//        ->form()
+//        ->fill('purchasables[1][qty]', 10)
+//        ->submit()
+//        ->assertRedirect();
+
+
+    /** @var \markhuot\craftpest\test\TestCase $test */
+    $test = test();
+
+    $test->post('/actions/commerce/cart/update-cart', [
+        'purchasableId' => $product->defaultVariant->id,
+        'qty' => 10,
+    ]);
+
+    $cart = \craft\commerce\Plugin::getInstance()->getCarts()->getCart();
+
+    expect($cart->lineItems[0]->qty)->toBe(10);
+});
